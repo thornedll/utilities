b6a1715e316cc4d3cf4e3f3cc25aece5ef6cce56
Dmitry <perov_dima11@bk.ru>
2025-03-16 11:03:18 +0300

    init commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from "react-router";
+import DateConverter from "./components/pages/DateConverter";
+import JsonConverter from "./components/pages/JsonConverter";
+import JsonDiff from "./components/pages/JsonDiff";
+import Navigation from "./components/blocks/Navigation";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Utilities</h1>
+      <Router>
+        <Navigation />
+        <Routes>
+          <Route path="/" element={<DateConverter />}></Route>
+          <Route path="/jsonConverter" element={<JsonConverter />}></Route>
+          <Route path="/jsonDiff" element={<JsonDiff />}></Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/components/blocks/Navigation.tsx
@@ -0,0 +1,18 @@
+import { NavLink } from "react-router";
+import styles from "./styles.module.scss";
+
+export default function Navigation() {
+  return (
+    <nav className={styles.nav}>
+      <NavLink to="/" className={styles.navLink}>
+        Date Converter
+      </NavLink>
+      <NavLink to="/jsonConverter" className={styles.navLink}>
+        JSON Converter
+      </NavLink>
+      <NavLink to="/jsonDiff" className={styles.navLink}>
+        JSON Diff
+      </NavLink>
+    </nav>
+  );
+}
