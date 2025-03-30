bf0172f792d8322e40149c8330cbb22be249950b
Dmitry <perov_dima11@bk.ru>
2025-03-30 12:12:35 +0300

    changes for gh-pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router";
+import { Route, Routes } from "react-router";
 import { DateConverter, JsonConverter, JsonDiff } from "./components/pages";
 import { Navigation } from "./components/blocks";
 import styles from "./assets/styles/global.module.scss";
@@ -6,17 +6,15 @@ import styles from "./assets/styles/global.module.scss";
 function App() {
   return (
     <div className="App">
-      <Router>
-        <div className={styles.header}>
-          <h1>Utilities</h1>
-          <Navigation />
-        </div>
-        <Routes>
-          <Route path="/" element={<DateConverter />}></Route>
-          <Route path="/jsonConverter" element={<JsonConverter />}></Route>
-          <Route path="/jsonDiff" element={<JsonDiff />}></Route>
-        </Routes>
-      </Router>
+      <div className={styles.header}>
+        <h1>Utilities</h1>
+        <Navigation />
+      </div>
+      <Routes>
+        <Route path="/" element={<DateConverter />}></Route>
+        <Route path="/jsonConverter" element={<JsonConverter />}></Route>
+        <Route path="/jsonDiff" element={<JsonDiff />}></Route>
+      </Routes>
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import styles from "styles.module.scss";
+import { BrowserRouter as Router } from "react-router-dom";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>
 );
 
